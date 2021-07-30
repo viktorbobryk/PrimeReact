d6c8b34821b36f031941164b0e59e2fde0ae1579
Bobryk <victor.bobryk@sigmaukraine.com>
2021-07-30 13:14:31 +0300

    added sidebar, menu, simple form, adapted text color to different themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,52 @@
 import React, { useRef, useState } from 'react';
 
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
-import logo from './primereact-logo.png';
-
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+
+import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
+import { TieredMenu } from 'primereact/tieredmenu';
 import classes from './App.module.css';
 
+import { menuItems } from './data/menuItems';
+
 function App() {
-  const [text, setText] = useState('');
+  const [activeSidebar, setActiveSidebar] = useState(false);
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
   const toastRef = useRef();
 
-  const onButtonClick = () => {
-    if (text) {
-      toastRef.current.show({ severity: 'success', summary: 'Success Message', detail: text });
-    } else {
-      toastRef.current.show({ severity: 'error', summary: 'Error Message', detail: 'Value is required' });
-    }
+  const submitForm = () => {
+    toastRef.current.show({ severity: 'info', summary: 'Welcome', detail: `${firstname} ${lastname}` });
   };
+
   return (
     <div className={classes.App}>
-      <Toast ref={toastRef} />
-      <header className={classes.AppHeader}>
-        <img className={classes.AppLogo} src={logo} alt="logo" />
-
-        <span className="p-float-label">
-          <InputText id="inputText" value={text} onChange={(e) => setText(e.target.value)} />
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="inputText" className="p-component">enter name</label>
-        </span>
-        <br />
-        <Button type="button" label="Submit" icon="pi pi-check" onClick={onButtonClick} />
-        {text}
-      </header>
+      <div className={`${classes.topBar} p-shadow-3 p-py-2 p-px-2`}>
+        <Button icon="pi pi-bars" className="p-button-rounded" onClick={() => setActiveSidebar(true)} />
+      </div>
+      <Sidebar visible={activeSidebar} onHide={() => setActiveSidebar(false)}>
+        <TieredMenu model={menuItems} />
+      </Sidebar>
+      <div className={`${classes.content} p-p-5`}>
+        <Toast ref={toastRef} />
+        <div className="p-fluid">
+          <div className="p-field">
+            <label htmlFor="firstname">Firstname</label>
+            <InputText id="firstname" type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+          </div>
+          <div className="p-field">
+            <label htmlFor="lastname">Lastname</label>
+            <InputText id="lastname" type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+          </div>
+        </div>
+        <Button label="Submit" icon="pi pi-check" onClick={submitForm} />
+      </div>
     </div>
   );
 }
